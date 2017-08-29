721640f2cc02f1f085446cf731fedec4a76825d5
Hilal Alsameraaii <hilal.al-sameraaii@cgs.act.edu.au>
2017-08-29 12:38:09 +1000

    Initial Commit

---
+++ LocalShopping/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LocalShopping
+//
+//  Created by Hilal Alsameraaii on 29/8/17.
+//  Copyright © 2017 Hilal Al-Sameraaii. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
