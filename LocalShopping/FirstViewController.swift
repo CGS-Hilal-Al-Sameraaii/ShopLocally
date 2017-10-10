b99267cda22ce3f78d011022fed39b1ae21b15a6
Hilal Alsameraaii <hilalalsameraaii@Hilals-MacBook-Pro.local>
2017-10-10 12:25:19 +1100

    So far, so bad

--- LocalShopping/FirstViewController.swift
+++ LocalShopping/FirstViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var searchBar: UITextField!
+    var searchQuery: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        searchQuery = textField.text
+        textField.resignFirstResponder()
+        
+        performSegue(withIdentifier: "ItemsSearch", sender: self)
+        
+        return true
     }
-
 
 }
 
+class ItemsResults: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var items = ["food", "music", "etc"]
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(items.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return(cell)
+    }
+    
+    
+    
+}
--- LocalShopping/SecondViewController.swift
+++ LocalShopping/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  FirstViewController.swift
 //  LocalShopping
 //
 //  Created by Hilal Alsameraaii on 29/8/17.
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var searchBar: UITextField!
+    var searchQuery: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        searchQuery = textField.text
+        textField.resignFirstResponder()
+        
+        performSegue(withIdentifier: "StoresSearch", sender: self)
+        
+        return true
     }
-
-
+    
 }
 
+class StoresResults: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var stores = ["localstore", "bigstore", "littlestore"]
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(stores.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = stores[indexPath.row]
+        
+        return(cell)
+    }
+    
+    
+    
+}
